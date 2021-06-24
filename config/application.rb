589d5953fbf342c64d38b49e44f1ae43a089e310
keisuke-5837 <86215950+keisuke-5837@users.noreply.github.com>
2021-06-24 17:11:02 +0900

    リセットCSSの設定

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,8 @@ module ChatApp
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
-      ghelper false
-      gtest_framework false
+      g.helper false
+      g.test_framework false
     end
   end
 end

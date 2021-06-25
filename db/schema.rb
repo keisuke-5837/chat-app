b4ca724ca973844eaa7ab0998f89a8f266f85e33
keisuke-5837 <86215950+keisuke-5837@users.noreply.github.com>
2021-06-25 11:26:31 +0900

    Revert "新規作成画面の表示"
    This reverts commit 7e5cce4f28cf841ace4367a0fdb638b1776f0529.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_25_021658) do
-
-  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_06_24_134116) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
